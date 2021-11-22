797ee474bc1fffefd0eedc7f5407a06584eea63b
Shaik Mahaboob jani <92629496+skmahaboobjani@users.noreply.github.com>
2021-11-22 09:21:30 -0800

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,16 @@
+function operatins(){
+  var a=10;
+  var b=20;
+   var add=a+b;
+   var sub=a-b;
+   var mul=a*b;
+   var div=a/b;
+
+  console.log( 'addition is :' +  add);
+  console.log( 'subtraction is :'  +  sub);
+  console.log( 'multiplication is :'  +  mul);
+  console.log('division is :' +   div)
+  
+}
+operatins()
+
---
+++ task2.js
@@ -0,0 +1,6 @@
+const a=10;
+{
+    const a=20;
+    console.log(a);
+}
+console.log(a);
